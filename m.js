703c529813e74e309f1667e9fad04f0d6f0d8910
loweyy <inesuni06@gmail.com>
2024-12-07 15:45:21 +0100

    first commit

---
+++ m.js
@@ -0,0 +1,295 @@
+const imagesData=[
+    { src: 'hrana/hrana (1).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (2).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (3).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (4).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (5).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (6).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (7).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (8).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (9).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (10).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (11).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (12).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (13).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (14).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (15).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (16).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (17).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (18).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (19).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (20).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (21).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (22).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (23).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (24).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (25).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (26).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (27).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (28).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (29).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'hrana/hrana (30).jpg', category: 'hrana', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (1).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (2).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (3).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (4).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (5).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (6).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (7).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (8).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (9).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (10).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (11).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (12).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (13).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (14).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (15).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (16).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (17).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (18).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (19).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (20).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (21).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (22).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (23).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (24).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (25).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (26).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (27).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (28).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (29).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'zivotinje/zivotinje (30).jpg', category: 'zivotinje', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (1).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (2).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (3).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (4).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (5).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (6).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (7).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (8).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (9).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (10).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (11).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (12).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (13).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (14).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (15).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (16).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (17).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (18).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (19).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (20).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (21).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (22).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (23).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (24).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (25).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (26).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (27).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (28).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (29).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'arhitektura/arhitektura (30).jpg', category: 'arhitektura', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (1).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (2).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (3).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (4).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (5).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (6).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (7).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (8).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (9).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (10).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (11).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (12).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (13).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (14).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (15).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (16).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (17).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (18).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (19).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (20).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (21).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (22).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (23).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (24).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (25).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (26).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (27).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (28).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (29).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'ljudi/ljudi (30).jpg', category: 'ljudi', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (1).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (2).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (3).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (4).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (5).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (6).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (7).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (8).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (9).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (10).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (11).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (12).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (13).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (14).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (15).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (16).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (17).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (18).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (19).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (20).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (21).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (22).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (23).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (24).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (25).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (26).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (27).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (28).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (29).jpg', category: 'pejzaz', likes: 0, comments: [] },
+    { src: 'pejzaz/pejzaz (30).jpg', category: 'pejzaz', likes: 0, comments: [] }
+];
+
+
+const imageGallery = document.getElementById('image-gallery');
+const imageModal = document.getElementById('image-modal');
+const modalImage = document.getElementById('modal-image');
+const likeButton = document.getElementById('like-button');
+const likeCount = document.getElementById('like-count');
+const commentInput = document.getElementById('comment-input');
+const commentButton = document.getElementById('comment-button');
+const commentsList = document.getElementById('comments-list');
+const saveToFavoritesButton = document.getElementById('save-to-favorites');
+const favoritesModal = document.getElementById('favorites-modal');
+const favoritesGallery = document.getElementById('favorites-gallery');
+const profileIcon = document.getElementById('profile-icon');
+const profileImageInput = document.getElementById('profile-image-input');
+const changeUsernameButton = document.getElementById('change-username-button');
+const usernameInput = document.getElementById('username-input');
+
+
+let userProfileImage = 'default-user.png'; 
+let userName = 'Korisnik'; 
+
+
+
+function loadImages() {
+    imageGallery.innerHTML = '';
+    imagesData.forEach((image, index) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.src;
+        imgElement.classList.add('image-item');
+        imgElement.addEventListener('click', () => openModal(image, index));
+        imageGallery.appendChild(imgElement);
+    });
+}
+
+function openModal(image, index) {
+    modalImage.src = image.src;
+    likeCount.textContent = image.likes;
+    commentsList.innerHTML = ''; 
+
+    image.comments.forEach(comment => {
+        const commentDiv = document.createElement('div');
+        commentDiv.classList.add('comment');
+        commentDiv.innerHTML = `
+            <img src="${userProfileImage}" alt="Profilna slika">
+            <div class="comment-text">${comment}</div>
+        `;
+        commentsList.appendChild(commentDiv);
+    });
+
+    likeButton.onclick = () => {
+        image.likes++;
+        likeCount.textContent = image.likes;
+    };
+
+    // rfr klik
+    commentButton.onclick = () => {
+        if (commentInput.value.trim() !== "") {
+            image.comments.push(commentInput.value.trim());
+            commentInput.value = ''; 
+            //rfr
+            const newCommentDiv = document.createElement('div');
+            newCommentDiv.classList.add('comment');
+            newCommentDiv.innerHTML = `
+                <img src="${userProfileImage}" alt="Profilna slika">
+                <div class="comment-text">${image.comments[image.comments.length - 1]}</div>
+            `;
+            commentsList.appendChild(newCommentDiv);
+        }
+    };
+
+    saveToFavoritesButton.onclick = () => {
+        saveToFavorites(image);
+    };
+
+    imageModal.style.display = 'flex';
+}
+
+function saveToFavorites(image) {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    favorites.push(image);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    loadFavorites();
+}
+
+function loadFavorites() {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    favoritesGallery.innerHTML = '';
+    favorites.forEach(favorite => {
+        const imgElement = document.createElement('img');
+        imgElement.src = favorite.src;
+        imgElement.classList.add('favorite-image');
+        favoritesGallery.appendChild(imgElement);
+    });
+}
+
+document.getElementById('heart-icon').onclick = () => {
+    favoritesModal.style.display = 'flex';
+};
+
+document.getElementById('close-modal').onclick = () => {
+    imageModal.style.display = 'none';
+};
+
+document.getElementById('close-favorites').onclick = () => {
+    favoritesModal.style.display = 'none';
+};
+
+document.getElementById('close-profile').onclick = () => {
+    profileModal.style.display = 'none';
+};
+
+profileIcon.onclick = () => {
+    profileModal.style.display = 'flex';
+};
+
+profileImageInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            userProfileImage = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+changeUsernameButton.addEventListener('click', () => {
+    const newUsername = usernameInput.value.trim();
+    if (newUsername) {
+        userName = newUsername;
+    }
+});
+
+loadImages();
+window.onscroll = function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Kada korisnik skroluje više od 50px
+        navbar.classList.add('shrink'); // Dodajte klasu 'shrink'
+    } else {
+        navbar.classList.remove('shrink'); // Uklonite klasu 'shrink' kada je na vrhu
+    }
+};
